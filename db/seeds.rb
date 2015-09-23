1bf88c8013368aaf72146dd0c4536b4c1024875c
Amer Mall <amer@icloud.com>
2015-09-22 23:40:53 -0400

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,12 @@ include RandomData
    )
  end
 
+# Add a post with a unique title and body to seeds.rb.
+# Before creating a unique post, check whether it already exists in the database.
+# Only seed the post if it doesn't already exist.
+# Use the find_or_create_by method.
+Post.find_or_create_by(title: 'unique title', body: 'unique body')
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
