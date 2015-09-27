396a09a65e184255bef243a795380a80e3e1f7fc
Amer Mall <amer@icloud.com>
2015-09-26 22:52:56 -0400

    checkpoint_35_crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    #we create an instance variable @post, then assign it an empty post returned by Post.new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+
+      flash[:notice] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
