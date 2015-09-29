d1e8c9baeaa965a8b8043449a57cecfe0dbf3a79
Amer Mall <amer@icloud.com>
2015-09-29 15:31:26 -0400

    Check point 37 completed, Topics and Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+  # def index
+  #   @posts = Post.all
+  # end
 
   def show
     @post = Post.find(params[:id])
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
 
   def new
     #we create an instance variable @post, then assign it an empty post returned by Post.new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -16,14 +17,17 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+ # #35
+    @post.topic = @topic
 
     if @post.save
 
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
 
-      flash[:notice] = "There was an error saving the post. Please try again."
+      flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
   end
@@ -39,7 +43,8 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      #redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try agan."
       render :edit
@@ -51,7 +56,8 @@ class PostsController < ApplicationController
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      #redirect_to posts_path
+      redirect_to @post.topic
     else
       flash[:error] = "There was an error deleting the post."
       render :show
